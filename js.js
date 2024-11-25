20df7e11e6243dba104ccb214d9350a7f3eed2e0
PedroMena <133895281+PietroMena@users.noreply.github.com>
2024-11-25 09:02:24 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,21 @@
+var menuButton = document.getElementById("menu-button");
+var menu = document.getElementById("menu");
+
+menuButton.addEventListener("click", function() {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+});
+
+document.querySelectorAll('.ancor').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
